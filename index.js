0ed9d46c8557a5f7504b801cbb97e543a0f8fbde
Hienba <96763849+Hienba@users.noreply.github.com>
2022-04-29 08:27:06 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Import packages
 const express = require("express");
-const home = require("./routes/home");
+const home = require("./routes/home.html");
 
 // Middlewares
 const app = express();
